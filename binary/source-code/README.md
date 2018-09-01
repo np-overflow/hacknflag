5ad8848a1d3d19db3c935308a2e50716bfb9b0b5
alloygoh <alloyg99@gmail.com>
2018-09-01 11:03:12 -0400

    Added source-code challenge

---
+++ binary/source-code/README.md
@@ -0,0 +1,6 @@
+# Source Code
+Difficulty of the challenge is 3/5.
+They say that using python to create executables is bad. Do you know why?
+
+## Writeup
+TODO
---
+++ binary/source-code/give-me-the-flag.py
@@ -0,0 +1,6 @@
+print "Welcome to my game.\nI hear you are looking for a flag.\nGive me the password and it shall be yours"
+password = raw_input("Enter the Password: ")
+if password == "Iamthelordoftheworld":
+    print "Here's your flag: HNF{H0w_d1D_y0u_se3_my_5OURc3_k0D3}"
+else:
+    print "nice try"
