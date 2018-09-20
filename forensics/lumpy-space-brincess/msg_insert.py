6c52712479c067f1b6857f9ce5b2fb997859f583
taufiqmmhd <taufiqmmhd@gmail.com>
2018-09-20 16:39:52 +0800

    Improve lumpy-space-brincess challenge
    Now message is dispersed further across the image

--- forensics/lumpy-space-brincess/msg_insert.py
+++ forensics/lumpy-space-brincess/msg_insert.py
@@ -4,23 +4,36 @@ random.seed(25)
 
 im = Image.open('ohmyglob.jpg')
 w, h = im.size[0], im.size[1]
-msg = "HNF{n0t_l3aSt_s1gnif1c4nt}"
+flag = "HNF{0h_My_gL0b_I_aM_th3_M0St_s1gN1f1cANt_pr1nCE55!!}"
 im_data = list(im.tobytes())
 
-# Complete data to encode into full image
-msg_to_encode = []
-for c in msg:
+# Bits of flag
+flag_bits_to_encode = []
+# Completed data to encode into full image
+msg_bits_to_encode = []
+for c in flag:
     bin_ver = bin(ord(c))[2:].zfill(8)
     for bit in bin_ver:
-        msg_to_encode.append(bit)
+        flag_bits_to_encode.append(bit)
 
 padding = "#"
 padding_bin = bin(ord(padding))[2:].zfill(8)
 
-# Generate noise
-while len(msg_to_encode) < w*h:
-    for bit in padding_bin:
-        msg_to_encode.append(bit)
+# Generate noise & fill up msg_bits_to_encode
+i = 0
+while len(msg_bits_to_encode) < w*h:
+    option = random.randint(0, 1000)
+    if i >= len(flag_bits_to_encode):
+        for bit in padding_bin:
+            msg_bits_to_encode.append(bit)
+    else:
+        if option < 999:
+            for bit in padding_bin:
+                msg_bits_to_encode.append(bit)
+        else:
+            for j in range(i, i+8):
+                msg_bits_to_encode.append(flag_bits_to_encode[j])
+            i += 8
 
 
 # Encode msg into LSBs of original image
@@ -28,7 +41,7 @@ j = 2
 for i in range(0, w*h):
     value_to_alter = im_data[j]
     bin_ver = list(bin(value_to_alter))
-    bin_ver[-1] = msg_to_encode[i]
+    bin_ver[-1] = msg_bits_to_encode[i]
     im_data[j] = int(''.join(bin_ver[2:]).zfill(8), 2)
     j += 3
 
