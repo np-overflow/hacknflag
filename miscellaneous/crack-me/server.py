0e95d058bf18870554853e8a604615e938f0d76c
alloygoh <alloyg99@gmail.com>
2018-09-21 11:02:22 -0400

    Add stdout flushing

--- miscellaneous/crack-me/server.py
+++ miscellaneous/crack-me/server.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2
+import sys
 
 password = "wbown"
 counter = 0
@@ -6,10 +7,12 @@ guess = False
 
 while not guess:
     print "Enter password for flag"
+    sys.stdout.flush()
 
     user = raw_input()
     counter += 1
     if user == password or counter >= 1000:
         print "Here's the flag: HNF{just_pur3_brut3_str3ngth!!}"
+        sys.stdout.flush()
         guess = True
 
